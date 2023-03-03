cc356538e2db0c7e3a4cdfac76f08fa65327fde6
Rafał Krawiec <r.krawiec@interactivevision.pl>
2023-03-03 15:42:46 +0100

    Add JSON_UNESCAPED_UNICODE flag for visitor data.

--- src/View/Directives/RenderVisitorApp.php
+++ src/View/Directives/RenderVisitorApp.php
@@ -11,7 +11,7 @@ class RenderVisitorApp
 
         $template = '
             <div id="' . $id . '"><?php echo $rendered; ?></div>
-            <script id="__VISITOR__" type="application/json"><?php echo json_encode($data, JSON_UNESCAPED_SLASHES); ?></script>
+            <script id="__VISITOR__" type="application/json"><?php echo json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE); ?></script>
         ';
 
         return implode('', array_map('trim', explode("\n", $template)));
